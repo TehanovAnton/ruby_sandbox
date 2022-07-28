1e0c23be00dd8531c3f850e405bd009c0e54d725
anton <tehanovanton@gmail.com>
2022-07-28 16:51:46 +0300

    Expose sign up params by overrideing registrations controller

--- device_token_auth_example/app/controllers/application_controller.rb
+++ device_token_auth_example/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nickname])
+  end
 end
---
+++ device_token_auth_example/app/controllers/overrides/registrations_controller.rb
@@ -0,0 +1,9 @@
+module Overrides
+  class RegistrationsController < DeviseTokenAuth::RegistrationsController
+    private
+
+    def sign_up_params
+      params.permit(:name, :nickname, :email, :password, :password_confirmation)
+    end
+  end
+end
