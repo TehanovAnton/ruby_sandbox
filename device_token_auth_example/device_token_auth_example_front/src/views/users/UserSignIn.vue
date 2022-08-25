16f513e008199c4b4d1892b9830f449cd96edc17
ormand89 <komoskoav@gmail.com>
2022-08-25 16:49:02 +0300

    sign up/in pages makes requests and rediret

--- device_token_auth_example/device_token_auth_example_front/src/views/users/UserSignIn.vue
+++ device_token_auth_example/device_token_auth_example_front/src/views/users/UserSignIn.vue
@@ -1,9 +1,36 @@
-<script setup></script>
+<script setup>
+  import { ref } from "vue";
+
+  const credentials = ref({})
+  async function authenticate(event) {
+    event.preventDefault();
+
+    const signInUrl =  'http://localhost:3000/auth/sign_in'
+    const signInOptions = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(credentials.value).toString(),
+    };
+
+    const response = await (await fetch(signInUrl, signInOptions));
+    if (response.ok) {
+      debugger
+      console.log('Great')
+    }
+  }
+</script>
 
 <template>
-  Email:<input type="text" name="email" />
-  <br />
+  <form @submit="authenticate">
+    Email:<input type="text" v-model="credentials.email" />
+    <br />
 
-  Password:<input type="text" name="password" />
-  <br />
+    Password:<input type="text" v-model="credentials.password" />
+    <br />
+
+    <button>Sign In</button>
+  </form>
 </template>
--- device_token_auth_example/device_token_auth_example_front/src/views/users/UserSignUp.vue
+++ device_token_auth_example/device_token_auth_example_front/src/views/users/UserSignUp.vue
@@ -1,35 +1,36 @@
 <script setup>
-import { ref } from "vue";
-import router from "../../router/router";
+  import { ref } from "vue";
+  import router from "../../router/router";
 
-const newUser = ref({
-  name: "Anton",
-  nickname: "Leichu",
-  email: "tehanovanton@gmail.com",
-  password: 12345678,
-  password_confirmation: 12345678,
-});
+  const newUser = ref({
+    name: "Anton",
+    nickname: "Leichu",
+    email: "tehanovanton@gmail.com",
+    password: 12345678,
+    password_confirmation: 12345678,
+  });
 
-async function signUp(event) {
-  event.preventDefault();
+  const signInView = () => router.push({ name: "signIn" });
 
-  const sign_up_url = "http://localhost:3000/auth";
-  const sign_up_options = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams(newUser.value).toString(),
-  };
+  async function signUp(event) {
+    event.preventDefault();
 
-  const response = await fetch(sign_up_url, sign_up_options);
-  if (response.ok) {
-    router.psuh({ name: "signIn" });
-  }
+    const signUpUrl = "http://localhost:3000/auth";
+    const signUpOptions = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(newUser.value).toString(),
+    };
+
+    const response = await fetch(signUpUrl, signUpOptions);
+    if (response.ok) {
+      debugger
+      signInView()
+    }
 }
-
-const signInVew = () => router.push({ name: "signIn" });
 </script>
 
 <template>
