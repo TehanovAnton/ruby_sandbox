ebf568dbc9ba9df40f3e461b2525ccca4f2c04e7
TehanovAnton <tehaonvanton@gmail.com>
2022-12-15 15:49:31 +0300

    figure becomes circle, triangle, ..., figure, and can calc square

--- paterns/strategy/square_calculator.rb
+++ paterns/strategy/square_calculator.rb
@@ -39,11 +39,11 @@ class Circle
   end
 end
 
-class FigureSquareCalculator
+class Figure
   attr_reader :figure
 
-  def initialize(figure)
-    @figure = figure
+  def initialize(figure_class, figure_params)
+    @figure = figure_class.new(*figure_params)
   end
 
   def calculate_square
@@ -51,9 +51,9 @@ class FigureSquareCalculator
   end
 end
 
-figures = [Circle.new(3), Triangle.new(2, 3), Square.new(2, 3)]
+figures = [{ class: Circle, params: [3] }, { class: Triangle, params: [2, 3]}, { class: Square, params: [2, 3]}]
 
 figures.each do |f|
-  fg_square_calc = FigureSquareCalculator.new(f)
-  puts "#{fg_square_calc.class}::#{fg_square_calc.figure.class} - #{fg_square_calc.calculate_square}"
+  fg = Figure.new(f[:class], f[:params])
+  puts "#{fg.class}::#{fg.figure.class} - #{fg.calculate_square}"
 end
